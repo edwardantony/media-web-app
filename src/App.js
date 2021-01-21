66f7553a8056a269e7fcf41236787822ce9d87f5
Junior Hlongwane <juniorWebProjects@gmail.com>
2021-01-21 13:48:03 +0200

    Add: Subscribers table listing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AdminListing } from "./components/Admin/AdminListing";
+import { SubscribersListing } from "./components/Admin/SubscribersListing";
 
 export const App = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/subscribers">
+          <SubscribersListing />
+        </Route>
         <Route path="/">
           <AdminListing />
         </Route>
